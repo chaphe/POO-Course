c160bb5dfa28e6503da7adc4e5842f338dfcbb40
reinaldopabonfuentes <reinaldopabonfuentes@users.noreply.github.com>
2017-06-07 15:59:46 -0500

    Actualización
    Se quita el parámetro de tipo Color

--- Polimorfismo/TP/Figuras-Polimorfismo-empty-Lienzo/src/co/edu/uis/fig/Triangulo.java
+++ Polimorfismo/TP/Figuras-Polimorfismo-empty-Lienzo/src/co/edu/uis/fig/Triangulo.java
@@ -6,29 +6,13 @@ import co.edu.uis.img.Imagen;
 
 public class Triangulo extends Figura {
 
-	public Triangulo(int x, int y, Color color, int base, int altura) {
+	public Triangulo(int x, int y, int base, int altura) {
 		
 	}
 
 	public void dibujar(Imagen img) {
 
-		int xf = getX() + base;
-		int yf = getY() + altura;
 
-		for (int i = getX(); i < xf; i++) {
-			img.setColor(i, yf, getColor());
-		}
-
-		for (int j = getY(); j < yf; j++) {
-			img.setColor(getY(), j, getColor());
-		}
-
-		// diagonal
-		for (double k = 0; k < 1.0; k += 0.01) {
-			double l = getX() + k * base;
-			double p = getY() + k * altura;
-			img.setColor((int) l, (int) p, getColor());
-		}
 		img.mostrar();
 	}
 }
